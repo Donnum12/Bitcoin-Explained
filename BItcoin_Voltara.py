bd570d0cd8246b045b95cd2501aa87a939b84c34
Donnum12 <38803912+Donnum12@users.noreply.github.com>
2018-05-08 23:21:00 -0500

    Majority Function. Dummy Code
    Currently has a bug in it and entire binary addition needs to be looked at.

--- BItcoin_Voltara.py
+++ BItcoin_Voltara.py
@@ -27,3 +27,41 @@ print(blocks_list);
 # Step 2. We will need to convert the first,second and third (0,1,2)
 # hashes into binary.
 #///////////////////////////////////////////////////////////////////
+# WARNING-----------------------------------------------------------
+# The Below Code is just dummy code talking about how the general
+# Process for the majority function. this is ideally what we connect
+# with the transformation function in order to create the full
+# Majority function.
+#///////////////////////////////////////////////////////////////////
+a = format(5, '07b')
+b = format(3, '07b')
+c = format(ord('e'), '07b')
+
+print(a)
+print(b)
+print(c)
+print(int(a) + int(b) + int(c));
+
+#///////////////////////////////////////////////////////////////////
+# Error Found, addition of bits done in integer form, not binary
+#///////////////////////////////////////////////////////////////////
+hash_1 = int(a) + int(b) + int(c)
+hash_1 = str(hash_1);
+
+token = [None]*len(hash_1);
+
+
+for x in range(0,len(hash_1)):
+    if int(hash_1[x]) >= 2:
+        token[x] = '1'
+    else:
+        token[x] = '0'
+print(token);
+
+new_string = ''.join(token)
+
+n = int(new_string, 2)
+n.to_bytes((n.bit_length() + 7) // 8, 'big').decode()
+print(n)
+#///////////////////////////////////////////////////////////////////
+
